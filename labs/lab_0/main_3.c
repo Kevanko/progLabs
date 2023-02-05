05413935e490576f6ea9b02bd9398c4d8aeb1379
Laber <Kevanko@users.noreply.github.com>
2023-02-05 21:17:12 +0700

    FIXED

--- labs/lab_0/main_3.c
+++ labs/lab_0/main_3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#define SQR(x) x* x
+#define SQR(x) x * x
 int main()
 {
     int y = 5;
-    int z = SQR(y + 1);
+    int z = SQR((y + 1));
     printf("z = %d\n", z);
     return 0;
 }
