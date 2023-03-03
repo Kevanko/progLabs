0363c1cce6ab8786ba59120f0270676829f1e57e
Николаенков Михаил Дмитриевич <iv222s16@bwp07.csc.local>
2023-03-03 15:22:00 +0700

    Change main.c

--- lab_2/src/main.c
+++ lab_2/src/main.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    IntVector* vector = int_vector_new(sizeof(int) * 5);
+    IntVector* vector = int_vector_new(5);
     int_vector_push_back(vector, 15);
 
     IntVector* vector2 = int_vector_copy(vector);
@@ -12,11 +12,18 @@ int main()
     for (int i = 0; i < vector->size; i++) {
         printf("%d", int_vector_get_item(vector, i));
     }
-    printf("\n");
+
+    int_vector_shrink_to_fit(vector2);
+    printf(" [%ld %ld]\n", vector->size, vector->capacity);
+    int_vector_push_back(vector2, 25);
+    int_vector_resize(vector2, 2);
+
+
     for (int i = 0; i < vector2->size; i++) {
         printf("%d ", int_vector_get_item(vector2, i));
     }
-    printf("\n");
+    printf(" [%ld %ld]\n", vector2->size, vector2->capacity);
+
     int_vector_free(vector);
     int_vector_free(vector2);
     return 0;
